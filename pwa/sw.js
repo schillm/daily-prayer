7f7498d0b21e22b0b0f3ba812241842a86176a1d
Matthew Schill <matthew.schill@gmail.com>
2026-03-30 18:51:19 -0500

    Bumped to v8

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'elca-prayer-v7';
+const CACHE_NAME = 'elca-prayer-v8';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
