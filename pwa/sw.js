a3f461aa926d2454a75adf8a9333f9c6db0663bb
Matthew Schill <matthew.schill@gmail.com>
2026-04-02 10:05:10 -0500

    Include optional bracketed verses in API fetch

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'elca-prayer-v9';
+const CACHE_NAME = 'elca-prayer-v10';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
